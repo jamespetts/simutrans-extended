e7fab9dd63dfac187e099b9ad80e30fdd0c3f15d
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2022-02-28 00:12:58 +0900

    FIX: Convoy loading bar is not restored correctly when restoring the dialog from autosave.

--- gui/convoi_info_t.cc
+++ gui/convoi_info_t.cc
@@ -1022,6 +1022,7 @@ void convoi_info_t::rdwr(loadsave_t *file)
 	// init window
 	if(  file->is_loading()  &&  cnv.is_bound())
 	{
+		loading_bar.set_cnv(cnv);
 		init(cnv);
 
 		reset_min_windowsize();
