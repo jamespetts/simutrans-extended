8f25e8f753e08941e7e1bade2280809a51a9f643
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2022-04-19 01:21:48 +0900

    ADD: Variable to return if the convoy includes a tilting vehicle

--- simconvoi.h
+++ simconvoi.h
@@ -1143,6 +1143,7 @@ public:
 	void reserve_own_tiles(bool unreserve = false);
 
 	bool has_tall_vehicles();
+	bool has_tilting_vehicles();
 
 	inline bool get_allow_clear_reservation() const { return allow_clear_reservation; }
 
