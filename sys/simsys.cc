b9cd1c7b1f8d57d4fb3f735171ce74d694afdd32
Markus Pristovsek <prissi@physik.tu-berlin.de>
2024-04-09 06:56:00 +0000

    CHG: Since we need anyway a wide string, jus use the windows font to ask for wide font names
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11129
    8aca7d54-2c30-db11-9de9-000461428c89
    
    # Conflicts:
    #	src/simutrans/sys/simsys.cc

--- sys/simsys.cc
+++ sys/simsys.cc
@@ -496,14 +496,15 @@ std::string dr_get_system_font()
 #ifdef WIN32
 #define DEFAULT_FONT "arial.ttf"
 
-	NONCLIENTMETRICS ncm;
-	ncm.cbSize = sizeof(NONCLIENTMETRICS);
-	SystemParametersInfo(SPI_GETNONCLIENTMETRICS, sizeof(NONCLIENTMETRICS), &ncm, 0);
-	std::wstring faceName(ncm.lfMessageFont.lfFaceName);
+	NONCLIENTMETRICSW ncm;
+	ncm.cbSize = sizeof(NONCLIENTMETRICSW);
+	SystemParametersInfoW(SPI_GETNONCLIENTMETRICS, sizeof(NONCLIENTMETRICSW), &ncm, 0);
+	std::wstring wsFaceName = ncm.lfMessageFont.lfFaceName;
+
 	const LPWSTR fontRegistryPath = L"Software\\Microsoft\\Windows NT\\CurrentVersion\\Fonts";
 	HKEY hKey;
 	LONG result;
-	std::wstring wsFaceName(faceName.begin(), faceName.end());
+
 	// Open Windows font registry key
 	result = RegOpenKeyExW(HKEY_LOCAL_MACHINE, fontRegistryPath, 0, KEY_READ, &hKey);
 	if (result != ERROR_SUCCESS) {
